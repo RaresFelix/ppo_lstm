fefef0aa1ff184d83f106f7125a3e2cb9091a24a
Felix Tudose <raresfelixtudose@gmail.com>
2025-01-14 09:46:03 +0000

    Add layer init; Add better function typing; Make rollout continue from last state instead of resetting (helpful for num_steps < avg episode lenght) Fix bug in hidden state reshaping Enable faster float32 matmul

--- ppo.py
+++ ppo.py
@@ -3,12 +3,13 @@ import random
 import time
 import torch.nn as nn
 import torch.optim as optim
+import einops
 from tqdm import tqdm
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 from jaxtyping import Int, Float, Array
 from torch import Tensor
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Callable
 from dataclasses import dataclass
 import gymnasium as gym
 
@@ -75,7 +76,17 @@ class RolloutBuffer():
         self.idx = 0
         self.full = False
     
-    def add(self, obs, done, act, log_prob, val, next_obs, next_done, rew, actor_hidden, critic_hidden):
+    def add(self, 
+            obs: Float[Tensor, "num_envs obs_dim"],
+            done: Float[Tensor, "num_envs"],
+            act: Float[Tensor, "num_envs"],
+            log_prob: Float[Tensor, "num_envs"],
+            val: Float[Tensor, "num_envs"],
+            next_obs: Float[Tensor, "num_envs obs_dim"],
+            next_done: Float[Tensor, "num_envs"],
+            rew: Float[Tensor, "num_envs"],
+            actor_hidden: Float[Tensor, "2 num_envs hidden_size"],
+            critic_hidden: Float[Tensor, "2 num_envs hidden_size"]) -> None:
         obs = torch.as_tensor(obs, dtype=torch.float32, device=self.device)
         done = torch.as_tensor(done, dtype=torch.float32, device=self.device)
         act = torch.as_tensor(act, dtype=torch.float32, device=self.device)
@@ -104,7 +115,10 @@ class RolloutBuffer():
             self.idx = 0
             self.full = True
 
-    def add_last(self, next_done, next_value, last_critic_hidden):
+    def add_last(self, 
+                next_done: Float[Tensor, "num_envs"],
+                next_value: Float[Tensor, "num_envs"],
+                last_critic_hidden: Float[Tensor, "2 num_envs hidden_size"]) -> None:
         #this is for gae calculation
         self.last_done = torch.as_tensor(next_done, dtype=torch.float32, device=self.device) 
         self.last_value = torch.as_tensor(next_value, dtype=torch.float32, device=self.device)
@@ -114,7 +128,7 @@ class RolloutBuffer():
         self.idx = 0
         self.full = False
     
-    def get_batch(self):
+    def get_batch(self) -> list[Float[Tensor, "..."]]:
         with torch.no_grad():
             if self.full:
                 self.advantages = compute_gae(self.args, self.rewards, self.values, self.dones, self.last_done, self.last_value)
@@ -131,7 +145,12 @@ def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
     nn.init.constant_(layer.bias, bias_const)
     return layer
 
-def compute_gae(args, rewards: Float[Tensor, 'num_steps num_envs'], values, dones, last_dones, last_values: Float[Tensor, 'num_envs']):
+def compute_gae(args: Args,
+                rewards: Float[Tensor, "num_steps num_envs"],
+                values: Float[Tensor, "num_steps num_envs"],
+                dones: Float[Tensor, "num_steps num_envs"],
+                last_dones: Float[Tensor, "num_envs"],
+                last_values: Float[Tensor, "num_envs"]) -> Float[Tensor, "num_steps num_envs"]:
     gamma = args.gamma
     gae_lambda = args.gae_lambda
 
@@ -143,7 +162,7 @@ def compute_gae(args, rewards: Float[Tensor, 'num_steps num_envs'], values, done
     advantages = deltas.clone()
     
     factors = gamma * gae_lambda * (1 - all_dones[:-1])
-    for t in range(deltas.size(1) - 2, -1, -1):
+    for t in range(deltas.size(0) - 2, -1, -1):
         advantages[t] += factors[t] * advantages[t + 1]
 
     return advantages
@@ -157,22 +176,31 @@ class Actor(nn.Module):
                 nn.init.constant_(param, 0)
         #self.fc = nn.Linear(args.hidden_size, act_dim)
         self.fc = nn.Sequential(
-            nn.Linear(args.hidden_size, 128),
+            layer_init(nn.Linear(args.hidden_size, 128)),
             nn.Tanh(),
-            nn.Linear(128, 128),
+            layer_init(nn.Linear(128, 128)),
             nn.Tanh(),
-            nn.Linear(128, act_dim)
+            layer_init(nn.Linear(128, act_dim), std=.01)
         )
         self.args = args
     
-    def forward(self, x: Float[Tensor, 'batch obs_dim'], hidden: Tuple[Tensor, Tensor]) -> Tuple[Tensor, Tuple[Tensor, Tensor]]:
+    def forward(self,
+                x: Float[Tensor, "batch obs_dim"],
+                hidden: Tuple[Float[Tensor, "batch hidden_size"], Float[Tensor, "batch hidden_size"]]
+                ) -> Tuple[Float[Tensor, "batch act_dim"], 
+                          Tuple[Float[Tensor, "batch hidden_size"], Float[Tensor, "batch hidden_size"]]]:
         #lst expects (batch, seq, features) but here seq = 1
         hidden = self.lstm(x, (hidden[0], hidden[1]))
         hidden = torch.stack(hidden)
         y = self.fc(hidden[0])
         return y, hidden
     
-    def get_action(self, x: Float[Tensor, 'batch obs_dim'], hidden: Tuple[Tensor, Tensor]) -> Tuple[Tensor, Tensor]:
+    def get_action(self,
+                   x: Float[Tensor, "batch obs_dim"],
+                   hidden: Tuple[Float[Tensor, "batch hidden_size"], Float[Tensor, "batch hidden_size"]]
+                   ) -> Tuple[Float[Tensor, "batch"],
+                            Float[Tensor, "batch"],
+                            Tuple[Float[Tensor, "batch hidden_size"], Float[Tensor, "batch hidden_size"]]]:
         y, hidden = self(x, hidden)
         dist = torch.distributions.Categorical(logits=y)
         act = dist.sample()
@@ -185,14 +213,18 @@ class Critic(nn.Module):
         self.lstm = nn.LSTMCell(obs_dim, args.hidden_size)
         #self.fc = nn.Linear(args.hidden_size, 1)
         self.fc = nn.Sequential(
-            nn.Linear(args.hidden_size, 128),
+            layer_init(nn.Linear(args.hidden_size, 128)),
             nn.Tanh(),
-            nn.Linear(128, 128),
+            layer_init(nn.Linear(128, 128)),
             nn.Tanh(),
-            nn.Linear(128, 1)
+            layer_init(nn.Linear(128, 1), std=1.)
         )
     
-    def forward(self, x: Float[Tensor, 'batch obs_dim'], hidden: Tuple[Tensor, Tensor]) -> Tuple[Tensor, Tuple[Tensor, Tensor]]:
+    def forward(self,
+                x: Float[Tensor, "batch obs_dim"],
+                hidden: Tuple[Float[Tensor, "batch hidden_size"], Float[Tensor, "batch hidden_size"]]
+                ) -> Tuple[Float[Tensor, "batch"],
+                          Tuple[Float[Tensor, "batch hidden_size"], Float[Tensor, "batch hidden_size"]]]:
         hidden = self.lstm(x, (hidden[0], hidden[1]))
         hidden = torch.stack(hidden)
         y = self.fc(hidden[0]).squeeze(-1)                                                                                                                                                                                                                      
@@ -211,6 +243,7 @@ class Agent():
         
         self.actor = Actor(self.obs_dim, self.act_dim, args).to(device)
         self.critic = Critic(self.obs_dim, args).to(device)
+    
 
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=args.learning_rate)
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=args.learning_rate)
@@ -219,14 +252,25 @@ class Agent():
 
         self.buffer = RolloutBuffer(self.obs_dim, args)
 
-    def rollout(self):
-        observation = self.envs.reset()[0]
-        done = np.zeros(self.args.num_envs)
+    @torch.inference_mode()
+    def rollout(self) -> None:
+        self.critic.eval()
+        self.actor.eval()
 
-        actor_hidden = torch.zeros(2, self.args.num_envs, self.args.hidden_size, device=self.device)
-        critic_hidden = torch.zeros(2, self.args.num_envs, self.args.hidden_size, device=self.device)
+        if self.step == 0:
+            observation = self.envs.reset()[0]
+            done = np.zeros(self.args.num_envs)
+            actor_hidden = torch.zeros(2, self.args.num_envs, self.args.hidden_size, device=self.device)
+            critic_hidden = torch.zeros(2, self.args.num_envs, self.args.hidden_size, device=self.device)
+        else:
+            observation = self.last_observation
+            done = self.last_done
+            actor_hidden = self.last_actor_hidden
+            critic_hidden = self.last_critic_hidden
 
         self.buffer.reset()
+        freq_terminal_log = 2
+        cnt_terminal_log = 0
 
         for step in range(self.args.num_steps):
             obs_tensor = torch.tensor(observation, dtype=torch.float32, device=self.device)
@@ -240,16 +284,17 @@ class Agent():
             next_done = np.logical_or(termination, truncation)
 
             if 'episode' in infos and self.writer and infos['_episode'].sum():
-                avg_returns = infos['episode']['r'].sum() / infos['_episode'].sum()
-                avg_lengths = infos['episode']['l'].sum() / infos['_episode'].sum()
-                number_terminals = infos['_episode'].sum()
+                cnt_terminal_log += 1
+                if cnt_terminal_log % freq_terminal_log == 0:
+                    avg_returns = infos['episode']['r'].sum() / infos['_episode'].sum()
+                    avg_lengths = infos['episode']['l'].sum() / infos['_episode'].sum()
+                    number_terminals = infos['_episode'].sum()
 
-                self.writer.add_scalar('episode/return', avg_returns, self.step)
-                self.writer.add_scalar('episode/length', avg_lengths, self.step)
-                self.writer.add_scalar('episode/terminals', number_terminals, self.step)
+                    self.writer.add_scalar('episode/return', avg_returns, self.step)
+                    self.writer.add_scalar('episode/length', avg_lengths, self.step)
+                    self.writer.add_scalar('episode/terminals', number_terminals, self.step)
 
-            with torch.no_grad():
-                value, next_critic_hidden = self.critic(obs_tensor, critic_hidden)
+            value, next_critic_hidden = self.critic(obs_tensor, critic_hidden)
             
             self.buffer.add(obs_tensor, done, action, log_prob, value, next_observation, next_done, reward, actor_hidden, critic_hidden)
 
@@ -265,15 +310,21 @@ class Agent():
 
             observation = next_observation
             done = next_done
-        
+
         obs_tensor = torch.tensor(observation, dtype=torch.float32, device=self.device)
-        with torch.no_grad():
-            next_value, last_critic_hidden = self.critic(obs_tensor, critic_hidden)
+        next_value, last_critic_hidden = self.critic(obs_tensor, critic_hidden)
         
         self.buffer.add_last(next_done, next_value, last_critic_hidden)
-        
+        self.actor.train()
+        self.critic.train()
 
-    def train(self):
+        self.last_observation = observation
+        self.last_done = done
+        self.last_actor_hidden = actor_hidden
+        self.last_critic_hidden = critic_hidden
+            
+
+    def train(self) -> None:
         progress_bar = tqdm(range(self.args.total_steps))
         self.step = 0
 
@@ -314,7 +365,8 @@ class Agent():
                     
                     # critic accpts only one batch size, so we flatten
                     obs_view = observations.view(-1, observations.shape[-1])
-                    critic_hidden_view = critic_hidden.view(2, -1, critic_hidden.shape[-1])
+                    #critic_hidden_view = critic_hidden.view(2, -1, critic_hidden.shape[-1])
+                    critic_hidden_view = einops.rearrange(critic_hidden, 'batch n2 num_envs hidden_size -> n2 (batch num_envs) hidden_size')
                     values_pred_view, _ = self.critic(obs_view, critic_hidden_view)
 
                     values_pred = values_pred_view.view(-1, self.args.num_envs) # back to (batch_size, num_envs)
@@ -325,9 +377,9 @@ class Agent():
                     VF_loss = VF_deltas.pow(2).sum() / ((1 - dones).sum() + 1e-5)
 
                     # Actor loss
-                    actor_hidden_view = actor_hidden.view(2, -1, actor_hidden.shape[-1])
+                    actor_hidden_view = einops.rearrange(actor_hidden, 'batch n2 num_envs hidden_size -> n2 (batch num_envs) hidden_size')
                     action_logits_view, _ = self.actor(obs_view, actor_hidden_view)
-                    action_logits = action_logits_view.view(-1, self.args.num_envs, self.act_dim)
+                    action_logits = einops.rearrange(action_logits_view, '(batch num_envs) act_dim -> batch num_envs act_dim', batch=self.args.minibatch_size)
                     dist = torch.distributions.Categorical(logits=action_logits)
                     entropy = dist.entropy().mean()
                     new_log_probs = dist.log_prob(actions)
@@ -368,7 +420,7 @@ class Agent():
             train_time = time.time() - time0
             print(f'Rollout time: {rollout_time:.2f}s, Train time: {train_time:.2f}s')
 
-def make_env(env_id: str, idx: int, record_video: bool = False):
+def make_env(env_id: str, idx: Int, record_video: bool = False) -> Callable:
     def thunk():
         env = gym.make(env_id)
         env = gym.wrappers.RecordEpisodeStatistics(env)
@@ -377,7 +429,8 @@ def make_env(env_id: str, idx: int, record_video: bool = False):
         return env
     return thunk
 
-def main():
+def main() -> None:
+    torch.set_float32_matmul_precision('high')
     args = Args()
     run_name = f'{args.project_name}_{args.env_id}_{int(time.time())}'
     writer = SummaryWriter('runs/' + run_name)
